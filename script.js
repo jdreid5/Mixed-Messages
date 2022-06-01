dceae3fdc67f39c1fca876a7f6fc08b8c1785d7e
jdreid5 <jdreid5@aol.com>
2022-06-01 14:04:10 +0100

    add comments

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
+// three arrays containing personality traits
 const words1 = ['funny', 'bubbly', 'silly', 'driven', 'chaotic'];
 const words2 = ['smart', 'shrewd', 'predictable', 'reliable', 'demonic'];
 const words3 = ['hopeful', 'friendly', 'sociable', 'romantic', 'reserved'];
 
+// function combining the three arrays into one array with a random element selected from each
 const randomSelector = (arr1, arr2, arr3) => {
     let newArr = [arr1[Math.floor(Math.random() * 5)], arr2[Math.floor(Math.random() * 5)], arr3[Math.floor(Math.random() * 5)]];
     return newArr;
 };
 
+// primary function outputting interpolated statement containing random combination of all words
 const mixedMessages = (array) => {
     const arrNum = Math.floor(Math.random() * 3);
     const word1 = array[arrNum];
@@ -18,4 +21,5 @@ const mixedMessages = (array) => {
     console.log(`You are a ${word1} person, but you have a tendency to be ${word2} at times, and yet you think you are a ${word3} person.`)
 };
 
+// function call
 mixedMessages(randomSelector(words1, words2, words3));
